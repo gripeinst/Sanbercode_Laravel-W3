43ad49e95111f991abc0aa90a91060759653098f
GripEinst <52878810+gripeinst@users.noreply.github.com>
2020-07-01 16:26:03 +0700

    Laravel MVC

---
+++ TugasMVC/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<html>
+    <head>
+        <title>Selamat Datang!</title>
+    </head>
+    <body>
+        <h1> SELAMAT DATANG! {{$fname}} {{$lname}}</h1>
+        <h2>Terima kasih telah bergabung dengan SanberBook. Social media kita bersama!</h2>
+    </body>
+</html>
